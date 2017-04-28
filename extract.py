5b35eeacfb0885a0832263f9e8e1e72bb61d42e9
Gurpreet Singh <gdhuper@gmail.com>
2017-04-27 21:27:09 -0700

    updated scrited to run with cmd args

--- extract.py
+++ extract.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-for root, dirs, files in os.walk('lfw/'):  
+for root, dirs, files in os.walk(sys.argv[0]):  
    for file in files:
       path_file = os.path.join(root,file)
-      shutil.copy2(path_file,'img/') 
+      shutil.copy2(path_file,sys.argv[1]) 
